59b3208fa77b9bf0ff9972656213279b2e50afcb
Fixfon <12512757+fixfon@users.noreply.github.com>
2023-05-04 14:47:33 +0300

    Changed environment variable typings.

--- types/environment.d.ts
+++ types/environment.d.ts
@@ -8,11 +8,10 @@ declare global {
 			OPENAI_KEY_DEV: string;
 			OPENAI_KEY_PROD: string;
 			OPENAI_ORG_ID: string;
-			NOTION_TOKEN: string;
-			NOTION_DATABASE_ID: string;
 			DEVELOPER_ID_LIST: string[];
 			SUPPORT_SERVER_INVITE: string;
 			SUPPORT_SERVER_ID: string;
+			DATABASE_URL: string;
 		}
 	}
 }
