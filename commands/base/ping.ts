10848a575872e7a63a77e87aa8a69d5e05368de6
Fixfon <12512757+fixfon@users.noreply.github.com>
2023-06-28 16:37:58 +0300

    Fixed gitignore for cat genre file reading

--- commands/base/ping.ts
+++ commands/base/ping.ts
@@ -1,17 +1,27 @@
 import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
 import type { ICommand } from '../../types/ICommand';
-import { database } from '../../structures/Database';
-import * as movieGenres from './movieGenres.json';
 
 const ping: ICommand = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Provides information about the user.'),
+	// .addStringOption((option) =>
+	// 	option
+	// 		.setName('name')
+	// 		.setDescription('Your name')
+	// 		.addChoices(
+	// 			...genres.map((genre) => {
+	// 				return {
+	// 					name: genre.name,
+	// 					value: genre.name,
+	// 				};
+	// 			})
+	// 		)
+	// ),
 	adminOnly: false,
 	async execute(interaction: CommandInteraction) {
 		await interaction.deferReply();
 
-		await database.updateAllMovieGenres(movieGenres);
 		await interaction.editReply(
 			`This command was run by ${interaction.user.username}, who joined on ${interaction.member}.`
 		);
