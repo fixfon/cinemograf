aa577b5633ed412c7d87280df19da3b0236831b1
Fixfon <12512757+fixfon@users.noreply.github.com>
2023-07-09 20:37:10 +0300

    Moved prompts to env variables.

--- types/environment.d.ts
+++ types/environment.d.ts
@@ -3,6 +3,7 @@ export {};
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
+			DATABASE_URL: string;
 			DISCORD_TOKEN: string;
 			DISCORD_CLIENT_ID: string;
 			OPENAI_KEY_DEV: string;
@@ -11,13 +12,14 @@ declare global {
 			DEVELOPER_ID_LIST: string[];
 			SUPPORT_SERVER_INVITE: string;
 			SUPPORT_SERVER_ID: string;
-			DATABASE_URL: string;
 			GOOGLE_CLIENT_ID: string;
 			GOOGLE_SERVICE_ACCOUNT: string;
 			GOOGLE_SERVICE_PRIVATE: string;
 			GOOGLE_SERVICE_SCOPE: string;
 			GOOGLE_SPREADSHEET_ID: string;
 			GOOGLE_SHEET_NAME: string;
+			AI_OUTPUT_PROMPT: string;
+			EMOJI_OUTPUT_PROMPT: string;
 		}
 	}
 }
